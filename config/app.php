5ba4b42c8ac5163e5ccde0a9b791963ed80968b9
Basilius Sauter <basilius.sauter@gmail.com>
2016-03-22 17:21:24 +0100

    Revert "Delete app.php"
    This reverts commit e27373038bfa244715ac05ca5a0e021eedc69e30.

---
+++ config/app.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * app configuration file
+ *
+ * @package Config
+ */
+
+
+return [
+	// Must Change
+	"id" => "kjhu7686zuihkiasd",
+    
+    // Could Change
+    "gametitle" => "NewLoGD",
+	
+	// Do not change
+	"version" => "0.0.1-dev",
+];
